c51aedb352be045e70222664967c491d92857d15
timothyckl <timothy.ckl@outlook.com>
2025-11-02 22:24:25 +0800

    refactor: modularised argument checking and file opening into functions

--- src/main.c
+++ src/main.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_LINE_LENGTH 256 
+#define MAX_LINE_LENGTH 256 // this number was arbitrarily chosen
+
+// function prototypes
+int check_args(int argc, char *argv[]);
+FILE *open_file(const char *file_path);
+// read_file(file_handle)
 
 int main(int argc, char *argv[]) {
-  // checks the number of command-line arguments
-  if (argc < 2) {
-    printf("Usage: %s <path-to-file>\n", argv[0]);
+  // check command line arguments
+  if (check_args(argc, argv) != 0) {
     return EXIT_FAILURE;
   }
 
-  const char *file_path = argv[1];
-
   // attempt to open file
-  FILE *file_handle;
-
-  file_handle = fopen(file_path, "r");
-
+  const char *file_path = argv[1];
+  FILE *file_handle = open_file(file_path);
+  
   if (file_handle == NULL) {
-    printf("Error opening file.\n");
     return EXIT_FAILURE;
   }
 
   printf("Success opening file!\n");
 
+  // reads file line by line
   char buffer[MAX_LINE_LENGTH];
   int current_line = 0;
 
-  // reads file line by line
   while (fgets(buffer, MAX_LINE_LENGTH, file_handle) != NULL) {
     current_line++;
     printf("Line %i: %s", current_line, buffer);
@@ -37,4 +37,23 @@ int main(int argc, char *argv[]) {
   fclose(file_handle);
 
   return EXIT_SUCCESS;
+}
+
+int check_args(int argc, char *argv[]){
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <path-to-file>\n", argv[0]);
+    return -1;
+  }
+  return 0;
+}
+
+FILE *open_file(const char *file_path) {
+  FILE *file_handle = fopen(file_path, "r");
+
+  if (file_handle == NULL) {
+    fprintf(stderr, "Error opening file.\n");
+    return NULL;
+  }
+
+  return file_handle;
 }
