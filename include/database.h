956eb98dde33a5095e460cabfb94b14218f8fe92
timothyckl <timothy.ckl@outlook.com>
2025-11-03 20:31:22 +0800

    add todo comments

--- include/database.h
+++ include/database.h
@@ -1,6 +1,7 @@
 #ifndef DATABASE_H 
 #define DATABASE_H 
 
+// our struct is defined here because out parser functions will be making use of it
 typedef struct {
   int id;
   char name[50];
@@ -8,5 +9,7 @@ typedef struct {
   float mark;
 } StudentRecord;
 
+// TODO: add something to store a collection of records + metadata
+// TODO: add functions to interact with our collection of records 
 
 #endif
