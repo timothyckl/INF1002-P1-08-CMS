832b547ae0e4ab3450caa8d24cf4f89d65aa85a4
timothyckl <timothy.ckl@outlook.com>
2025-11-03 20:42:03 +0800

    add notes

--- include/utils.h
+++ include/utils.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+// NOTES: not sure if this is the appropriate place for these functions
+//       but i'll leave them here for now :000
 int check_args(int argc, char *argv[]);
 FILE *get_file_handle(const char *file_path);
 void print_file_lines(FILE* handle, int buffer_size, bool show_line_num);
