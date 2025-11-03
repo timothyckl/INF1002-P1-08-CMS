bf2cb9cd551eea849247ffbfc098dce6c00c29f6
timothyckl <timothy.ckl@outlook.com>
2025-11-03 20:22:05 +0800

    add regex patterns and parser implmentation

--- src/parser.c
+++ src/parser.c
@@ -1,3 +1,15 @@
+#include "parser.h"
 
-// pattern for first few rows: Database Name:\s*(.+)
-// pattern for actual table: [^\t\r\n]+
+#define DB_NAME_PATTERN "Database Name:\s*(.+)" 
+#define AUTHOR_PATTERN "Authors:\s*(.+)"
+#define TABLE_PATTERN "Table Name:\s*(.+)"
+#define FIELD_PATTERN "[^\t\r\n]+"
+
+// TODO: check if line is empty
+// TODO: setup regex pattern
+// TODO: extract fields from line 
+// TODO: validate types (?)
+bool parse_line(const char *line, StudentRecord *out) {
+
+  return true;
+}
